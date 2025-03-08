6b13d1b526ef42b3cb02a5895a9b22009945ac6d
Poojasiva06 <poojasiva615@gmail.com>
2025-03-08 16:44:57 +0530

    DynamicPage Task3

---
+++ dynamicpage/src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>ℹ️ About </h1>
+      <p>I am from Krishnagiri.</p>
+      <p>I completed my BE ECE in Velammal Institute of Technology</p>
+      <p>I did my schooling in Sri Vidya Mandir MHSS</p>
+    </div>
+  );
+};
+
+export default About;
