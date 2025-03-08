b0357378a5d37b1193bbb21bf1e53c006e306b97
Poojasiva06 <poojasiva615@gmail.com>
2025-03-08 17:07:54 +0530

    Dynamic page

---
+++ dynamic/src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>📞 Contact Us</h1>
+      <p>You can reach us at <strong>contact@example.com</strong>.</p>
+    </div>
+  );
+};
+
+export default Contact;
